df2cde817bff495d62790662b85994d26a861a4d
Nelson <nelson.npl@gmail.com>
2024-06-11 17:20:13 -0400

    feat: add mensagens.js for error messages

---
+++ src/common/mensagens.js
@@ -0,0 +1,14 @@
+const mensagens = {
+    numeroDoDocumentoInvalido: 'Número do documento inválido',
+    tipoDeConexaoInvalido: 'Tipo de conexão inválido',
+    classeDeConsumoInvalida: 'Classe de consumo inválida',
+    modalidadeTarifariaInvalida: 'Modalidade tarifária inválida',
+    historicoDeConsumoInvalido: 'Histórico de consumo inválido',
+    classeDeConsumoNaoAceita: 'Classe de consumo não aceita',
+    modalidadeTarifariaNaoAceita: 'Modalidade tarifária não aceita',
+    consumoBaixoMonofasico: 'Consumo muito baixo para tipo de conexão monofásico',
+    consumoBaixoBifasico: 'Consumo muito baixo para tipo de conexão bifásico',
+    consumoBaixoTrifasico: 'Consumo muito baixo para tipo de conexão trifásico'
+};
+
+module.exports = mensagens;
